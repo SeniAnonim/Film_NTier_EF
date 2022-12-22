884a8bdab18cb651738c894b8e97da54251d077f
yahyaciftci <121261917+yahyaciftci@users.noreply.github.com>
2022-12-23 00:46:22 +0300

    Film_NTier
    Proje devam etmektedir.

---
+++ FilmBox_Proje/BLL/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using DAL;
+using DTO.FilmBoxDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Repositories
+{
+    public class UserRepository
+    {
+        FilmBox_DbEntities db = Db_Tools.DbInstance;
+        public List<UsersDTO> GetAll()
+        {
+           return db.Users.Select(c => new UsersDTO { UserID = c.usersID, UserName = c.KullaniciAdi, Password = c.Sifre }).ToList();
+        }
+        public void Delete(int id)
+        {
+            User gelen = db.Users.Find(id);
+            db.Users.Remove(gelen);
+            db.SaveChanges();
+        }
+
+        public UsersDTO GetByName(string kullanici,string sifre)
+        {
+          User gelen=  db.Users.Find(kullanici);
+            return new UsersDTO { UserName = gelen.KullaniciAdi,Password=gelen.Sifre };
+
+        }
+
+        public void Insert(UsersDTO item)
+        {
+            db.Users.Add(new User { KullaniciAdi = item.UserName, Sifre = item.Password});
+            db.SaveChanges();
+        }
+
+        public void Update(UsersDTO item)
+        {
+            User gelen = db.Users.Find(item.UserID);
+            gelen.Sifre = item.Password;
+            db.SaveChanges();
+        }
+    }
+}
---
+++ FilmBox_Proje/WinUI/Form1.cs
@@ -0,0 +1,52 @@
+﻿using BLL.Repositories;
+using DTO.FilmBoxDTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static string kullanici(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+            byte[] result = md5.Hash;
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                strBuilder.Append(result[i].ToString("x2"));    
+            }
+            return strBuilder.ToString();
+        }
+        UserRepository Urp = new UserRepository();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+           string ad= Urp.GetAll().Select(c=>c.UserName).ToString();
+           
+
+            
+        }
+    }
+}
+
